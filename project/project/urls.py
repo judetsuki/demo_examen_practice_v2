a6708dcd08edae7b3bbc231b81619ce96358638e
judetsuki <Luka9609@gmail.com>
2026-04-16 21:41:28 +0300

    changed models. changed import. renewing forms and urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,10 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('store/',include('project_app.urls')),
-    path('',RedirectView.as_view(url='/accounts/login/'))
+    path('',LoginView.as_view(temlpate_name = 'store/login.html'),name='login'),
+    path('logout/', LogoutView.as_view(),name='logout')
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- project/project_app/models.py
+++ project/project_app/models.py
@@ -1,19 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
 class Product(models.Model):
     sku = models.CharField(max_length=100,unique=True,verbose_name='Артикул')
     name = models.CharField(max_length=255,verbose_name='Наименование')
     price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Цена')
-    unit = models.ForeignKey('Unit',on_delete=models.PROTECT,related_name='products',verbose_name='Единица Измерения')
-    supplier = models.ForeignKey('Supplier', on_delete=models.PROTECT,related_name='products',verbose_name = 'Поставщик')
-    manufacturer = models.ForeignKey('Manufacturer', on_delete = models.PROTECT,related_name='products',verbose_name='Производитель')
-    category = models.ForeignKey('Category', on_delete= models.PROTECT,related_name='products',verbose_name='Категория')
+    unit = models.CharField(max_length=20,default = 'шт.',verbose_name='Единица Измерения') 
+    supplier = models.CharField(max_length=100,verbose_name = 'Поставщик')
+    manufacturer = models.CharField(max_length=100,verbose_name='Производитель')
+    category = models.CharField(max_length=100,verbose_name='Категория') 
     discount = models.PositiveIntegerField(default=0,verbose_name='Скидка')
     stock = models.PositiveBigIntegerField(default=0,verbose_name='Количество на складе')
     description = models.TextField(verbose_name='Описание')
-    image = models.CharField(max_length=255,blank=True)
+    image = models.ImageField(upload_to='images/',max_length=255,blank=True, null=True,verbose_name='Изображение')
     def __str__(self):
         return f"{self.sku} | {self.name}"
 
@@ -22,11 +25,11 @@ class Order(models.Model):
         ('Завершен', 'Завершен'),
         ('Новый', 'Новый')
     ]
+    order_number = models.CharField(max_length=20,unique=True,verbose_name="Номер Заказа")
     order_date = models.DateField(verbose_name='Дата заказа')
     delivery_date = models.DateField(verbose_name='Дата доставки')
     status = models.CharField(choices=STATUS_CHOICES, max_length=30, verbose_name='Статус заказа')
     delivery_address = models.ForeignKey('DeliveryPoint', on_delete=models.CASCADE,verbose_name='Адрес пункта выдачи')
-    receiver_name = models.CharField(max_length=50,verbose_name='Имя получателя')
     delivery_code = models.CharField(max_length=30,verbose_name='Код для получения')
     user = models.ForeignKey(User, on_delete=models.PROTECT,related_name='orders',null=True, blank=True,verbose_name='Клиент',)
     def __str__(self):
@@ -39,29 +42,9 @@ class OrderDetail(models.Model):
     def __str__(self):
         return str(self.order)
     
-
-class Supplier(models.Model):
-    name = models.CharField(max_length=40)
-    def __str__(self):
-        return self.name
-
-class Category(models.Model):
-    name = models.CharField(max_length=40,verbose_name='Категория')
-    def __str__(self):
-        return self.name
-
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=40)
-    def __str__(self):
-        return self.name
-
 class DeliveryPoint(models.Model):
     address = models.CharField(max_length=255)
     def __str__(self):
         return self.address
 
-class Unit(models.Model):
-    name = models.CharField(max_length=40)
-    def __str__(self):
-        return self.name
     
