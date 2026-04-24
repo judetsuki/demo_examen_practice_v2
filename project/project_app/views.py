ae8049a6b277bc56059ff9d3a1ab5079e0a311bd
judetsuki <Luka9609@gmail.com>
2026-04-24 20:09:12 +0300

    deleting/editing working

--- project/project_app/views.py
+++ project/project_app/views.py
@@ -6,37 +6,37 @@ from .forms import *
 def delete_order(request,pk):
     if request.user.is_superuser:
         get_object_or_404(Order,pk=pk).delete()
-    return redirect('product_list')
+    return redirect('project_app:product_list')
 
 def delete_product(request,pk):
     if request.user.is_superuser:
         get_object_or_404(Product,pk=pk).delete()
-    return redirect('product_list')
+    return redirect('project_app:product_list')
 
 def edit_product(request,pk):
     if not request.user.is_superuser:
-        return redirect('product_list')
+        return redirect('project_app:product_list')
     
     product = get_object_or_404(Product,pk=pk)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            return redirect('product_list')
+            return redirect('project_app:product_list')
     else:
         form = ProductForm(instance=product)
     return render (request, 'store/form_generic.html', {'form':form, 'title':'редактирование продукта'})
 
 def edit_order(request,pk):
     if not request.user.is_superuser:
-        return redirect('product_list')
+        return redirect('project_app:product_list')
     
     order = get_object_or_404(Order,pk=pk)
     if request.method == 'POST':
         form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
-            return redirect('product_list')
+            return redirect('project_app:product_list')
     else:
         form = OrderForm(instance=order)
     return render (request, 'store/form_generic.html', {'form':form, 'title':'редактирование заказов'})
@@ -47,7 +47,7 @@ def product_list(request):
     products = Product.objects.all()
     orders = []
 
-    if user.is_authenticated and user.is_staff or user.is_superuser:
+    if user.is_authenticated and (user.is_staff or user.is_superuser):
         search_query=request.GET.get('search')
         if search_query:
             products=products.filter(name__icontains=search_query)
